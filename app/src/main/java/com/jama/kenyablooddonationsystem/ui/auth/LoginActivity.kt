b7ae127f5e6d67cfe179f5a0132b0c1bbd60eb93
Jama Mohamed <jama3137@gmail.com>
2019-09-18 13:12:13 +0300

    Initial commit

---
+++ app/src/main/java/com/jama/kenyablooddonationsystem/ui/auth/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.jama.kenyablooddonationsystem.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jama.kenyablooddonationsystem.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
