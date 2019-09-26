572b91b27f8ca7fd0bc305c013aef9fc794924ca
Jama Mohamed <jama3137@gmail.com>
2019-09-26 10:11:58 +0300

    (Started) Set up the request activity

---
+++ app/src/main/java/com/jama/kenyablooddonationsystem/ui/request/Request.kt
@@ -0,0 +1,13 @@
+package com.jama.kenyablooddonationsystem.ui.request
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jama.kenyablooddonationsystem.R
+
+class Request : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_request)
+    }
+}
