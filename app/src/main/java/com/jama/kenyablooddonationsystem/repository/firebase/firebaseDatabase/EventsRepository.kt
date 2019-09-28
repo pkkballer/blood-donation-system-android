1597dedffe2ecc6f3e79b5365a67b89397fa882e
Jama Mohamed <jama3137@gmail.com>
2019-09-28 11:29:20 +0300

    Created the event repository

--- app/src/main/java/com/jama/kenyablooddonationsystem/repository/firebase/firebaseDatabase/EventsRepository.kt
+++ app/src/main/java/com/jama/kenyablooddonationsystem/repository/firebase/firebaseDatabase/EventsRepository.kt
@@ -1,63 +1,53 @@
 package com.jama.kenyablooddonationsystem.repository.firebase.firebaseDatabase
 
 import androidx.lifecycle.MutableLiveData
-import com.jama.kenyablooddonationsystem.repository.firebase.firebaseAuth.AuthenticationRepository
 import com.firebase.geofire.GeoFire
 import com.firebase.geofire.GeoLocation
 import com.firebase.geofire.GeoQuery
 import com.firebase.geofire.GeoQueryEventListener
 import com.google.firebase.database.*
-import com.jama.kenyablooddonationsystem.models.RequestModel
-import com.jama.kenyablooddonationsystem.utils.RegexUtil
-import kotlinx.android.synthetic.main.activity_request.*
+import com.jama.kenyablooddonationsystem.models.EventModel
+import com.jama.kenyablooddonationsystem.repository.firebase.firebaseAuth.AuthenticationRepository
 
-
-class GeofireRepository {
+class EventsRepository {
 
     private val authRepository = AuthenticationRepository()
-    private val database = FirebaseDatabase.getInstance()
-    private val regexUtil = RegexUtil()
+    private val database: FirebaseDatabase = FirebaseDatabase.getInstance()
+    private val databaseEventsRef = database.getReference("events")
+    private val eventGeoFireRef = database.getReference("geofire/events")
     private lateinit var geoFire: GeoFire
     private lateinit var geoQuery: GeoQuery
-    var requestModelList: MutableLiveData<MutableList<RequestModel>> = MutableLiveData(mutableListOf())
+    var eventModelList: MutableLiveData<MutableList<EventModel>> =
+        MutableLiveData(mutableListOf())
 
     private fun <T> MutableLiveData<T>.notifyObserver() {
         this.value = this.value
     }
 
-    private fun requestRef(): DatabaseReference {
-        val bloodType = regexUtil.getUserBloodType(authRepository.getFirebaseUser()?.displayName.toString())
-        return database.getReference("geofire/request/${bloodType}")
-    }
-
-    fun getRequest(key: String) {
-        val requestRef = database.getReference("requests/$key")
+    fun getEvent(key: String) {
+        val requestRef = databaseEventsRef.child(key)
         requestRef.addListenerForSingleValueEvent(object : ValueEventListener {
             override fun onCancelled(p0: DatabaseError) {}
-
             override fun onDataChange(p0: DataSnapshot) {
-                val requestModel = p0.getValue(RequestModel::class.java)
-                if (requestModel!!.active) {
-                    requestModelList.value!!.add(requestModel!!)
-                    requestModelList.notifyObserver()
+                val eventModel = p0.getValue(EventModel::class.java)
+                if (eventModel!!.active) {
+                    eventModelList.value!!.add(eventModel)
+                    eventModelList.notifyObserver()
                 }
             }
-
         })
     }
 
     fun listenToRequests(latlang: Map<String, Double>) {
-        geoFire = GeoFire(requestRef())
+        geoFire = GeoFire(eventGeoFireRef)
         geoQuery = geoFire.queryAtLocation(GeoLocation(latlang["lng"]!!, latlang["lat"]!!), 10.0)
         geoQuery.addGeoQueryEventListener(object : GeoQueryEventListener {
             override fun onKeyEntered(key: String, location: GeoLocation) {
-                getRequest(key)
+                getEvent(key)
             }
 
             override fun onKeyExited(key: String) {}
-
             override fun onKeyMoved(key: String, location: GeoLocation) {}
-
             override fun onGeoQueryReady() {
                 println("Geofire All initial data has been loaded and events have been fired!")
             }
@@ -67,6 +57,5 @@ class GeofireRepository {
             }
         })
     }
-
 
 }
--- app/src/main/java/com/jama/kenyablooddonationsystem/repository/firebase/firebaseDatabase/RequestRepository.kt
+++ app/src/main/java/com/jama/kenyablooddonationsystem/repository/firebase/firebaseDatabase/RequestRepository.kt
@@ -1,9 +1,14 @@
 package com.jama.kenyablooddonationsystem.repository.firebase.firebaseDatabase
 
 import androidx.lifecycle.MutableLiveData
+import com.firebase.geofire.GeoFire
+import com.firebase.geofire.GeoLocation
+import com.firebase.geofire.GeoQuery
+import com.firebase.geofire.GeoQueryEventListener
 import com.google.firebase.database.*
 import com.jama.kenyablooddonationsystem.models.RequestModel
 import com.jama.kenyablooddonationsystem.repository.firebase.firebaseAuth.AuthenticationRepository
+import com.jama.kenyablooddonationsystem.utils.RegexUtil
 import kotlinx.coroutines.*
 
 class RequestRepository {
@@ -11,13 +16,61 @@ class RequestRepository {
     private val authRepository = AuthenticationRepository()
     private val database: FirebaseDatabase = FirebaseDatabase.getInstance()
     private val databaseRequestRef = database.getReference("requests")
+    private val regexUtil = RegexUtil()
+    private lateinit var geoFire: GeoFire
+    private lateinit var geoQuery: GeoQuery
+    var requestModelList: MutableLiveData<MutableList<RequestModel>> =
+        MutableLiveData(mutableListOf())
     var showSnackbar: MutableLiveData<String> = MutableLiveData("")
     var showProgressbar: MutableLiveData<Boolean> = MutableLiveData(false)
 
-    fun viewedRequest(key: String) {
-        databaseRequestRef.child(key).runTransaction(object: Transaction.Handler {
-            override fun onComplete(p0: DatabaseError?, p1: Boolean, p2: DataSnapshot?) {}
+    private fun <T> MutableLiveData<T>.notifyObserver() {
+        this.value = this.value
+    }
 
+    private fun requestRef(): DatabaseReference {
+        val bloodType =
+            regexUtil.getUserBloodType(authRepository.getFirebaseUser()?.displayName.toString())
+        return database.getReference("geofire/request/${bloodType}")
+    }
+
+    fun getRequest(key: String) {
+        val requestRef = databaseRequestRef.child(key)
+        requestRef.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onCancelled(p0: DatabaseError) {}
+            override fun onDataChange(p0: DataSnapshot) {
+                val requestModel = p0.getValue(RequestModel::class.java)
+                if (requestModel!!.active) {
+                    requestModelList.value!!.add(requestModel)
+                    requestModelList.notifyObserver()
+                }
+            }
+        })
+    }
+
+    fun listenToRequests(latlang: Map<String, Double>) {
+        geoFire = GeoFire(requestRef())
+        geoQuery = geoFire.queryAtLocation(GeoLocation(latlang["lng"]!!, latlang["lat"]!!), 10.0)
+        geoQuery.addGeoQueryEventListener(object : GeoQueryEventListener {
+            override fun onKeyEntered(key: String, location: GeoLocation) {
+                getRequest(key)
+            }
+
+            override fun onKeyExited(key: String) {}
+            override fun onKeyMoved(key: String, location: GeoLocation) {}
+            override fun onGeoQueryReady() {
+                println("Geofire All initial data has been loaded and events have been fired!")
+            }
+
+            override fun onGeoQueryError(error: DatabaseError) {
+                System.err.println("Geofire There was an error with this query: $error")
+            }
+        })
+    }
+
+    fun viewedRequest(key: String) {
+        databaseRequestRef.child(key).runTransaction(object : Transaction.Handler {
+            override fun onComplete(p0: DatabaseError?, p1: Boolean, p2: DataSnapshot?) {}
             override fun doTransaction(mutableData: MutableData): Transaction.Result {
                 val request = mutableData.getValue(RequestModel::class.java)
                     ?: return Transaction.success(mutableData)
@@ -36,7 +89,7 @@ class RequestRepository {
 
     fun acceptRequest(key: String) {
         showProgressbar(true)
-        acceptedRequestRef().child(key).addListenerForSingleValueEvent(object: ValueEventListener {
+        acceptedRequestRef().child(key).addListenerForSingleValueEvent(object : ValueEventListener {
             override fun onCancelled(p0: DatabaseError) {}
 
             override fun onDataChange(snapshot: DataSnapshot) {
@@ -55,8 +108,8 @@ class RequestRepository {
         })
     }
 
-    fun incrementAcceptedRequest(key:String) {
-        databaseRequestRef.child(key).runTransaction(object: Transaction.Handler {
+    fun incrementAcceptedRequest(key: String) {
+        databaseRequestRef.child(key).runTransaction(object : Transaction.Handler {
             override fun onComplete(p0: DatabaseError?, p1: Boolean, p2: DataSnapshot?) {}
 
             override fun doTransaction(mutableData: MutableData): Transaction.Result {
