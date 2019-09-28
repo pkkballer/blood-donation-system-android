873a94d917bb0adc992cd7d7b8d99964196ac68f
Jama Mohamed <jama3137@gmail.com>
2019-09-28 20:39:02 +0300

    Setup the event layout

---
+++ app/src/main/java/com/jama/kenyablooddonationsystem/ui/event/EventActivity.kt
@@ -0,0 +1,13 @@
+package com.jama.kenyablooddonationsystem.ui.event
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jama.kenyablooddonationsystem.R
+
+class EventActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_event)
+    }
+}
